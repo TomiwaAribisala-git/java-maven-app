b7dab7e3624818e612d48d975a7beb6f29f32853
unknown <tomiwaaribisala@gmail.com>
2023-05-05 04:11:46 +0100

    add docker login details to terraform script

--- terraform/script.sh
+++ terraform/script.sh
@@ -4,6 +4,9 @@ sudo yum install -y docker
 sudo systemctl enable docker 
 sudo systemctl start docker  
 sudo usermod -aG docker ec2-user 
+username="dG9taXdhOTc6aiFxSzVGanYhVlpreG43"
+password="dG9taXdhOTc6aiFxSzVGanYhVlpreG43"
+sudo docker login -u "$username" -p "$password"
 
 # install docker compose 
 sudo curl -SL https://github.com/docker/compose/releases/download/v2.17.2/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
